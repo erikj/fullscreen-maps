174bea16b792be16b6ce56bf007267c19be38fd1
molecule <molecule@Fourier.local>
2011-04-23 21:49:13 -0600

    first commit

---
+++ geo.js
@@ -0,0 +1,58 @@
+
+// http://code.google.com/apis/maps/documentation/javascript/basics.html
+// The following example attempts to determine the user's location through
+// the W3C navigator.geolocation property first, attempts a Google Gears approach
+// second, and bails out if neither approach works.
+
+// Note that using Google Gears requires loading the Javascript
+// at http://code.google.com/apis/gears/gears_init.js
+
+var initialLocation;
+var siberia = new google.maps.LatLng(60, 105);
+var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+var browserSupportFlag =  new Boolean();
+
+function initialize() {
+  var myOptions = {
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.HYBRID
+  };
+  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+  
+  // Try W3C Geolocation (Preferred)
+  if(navigator.geolocation) {
+    browserSupportFlag = true;
+    navigator.geolocation.getCurrentPosition(function(position) {
+      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+      map.setCenter(initialLocation);
+    }, function() {
+      handleNoGeolocation(browserSupportFlag);
+    });
+  // Try Google Gears Geolocation
+  } else if (google.gears) {
+    eval('http://code.google.com/apis/gears/gears_init.js')
+    browserSupportFlag = true;
+    var geo = google.gears.factory.create('beta.geolocation');
+    geo.getCurrentPosition(function(position) {
+      initialLocation = new google.maps.LatLng(position.latitude,position.longitude);
+      map.setCenter(initialLocation);
+    }, function() {
+      handleNoGeoLocation(browserSupportFlag);
+    });
+  // Browser doesn't support Geolocation
+  } else {
+    browserSupportFlag = false;
+    handleNoGeolocation(browserSupportFlag);
+  }
+  
+  function handleNoGeolocation(errorFlag) {
+    if (errorFlag == true) {
+      alert("Geolocation service failed.");
+      initialLocation = newyork;
+    } else {
+      alert("Your browser doesn't support geolocation. We've placed you in New York.");
+      initialLocation = newyork;
+    }
+    map.setCenter(initialLocation);
+  }
+}
